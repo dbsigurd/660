d9b31f7a3f64023a66f7e7a47d8a0346683f9843
Devon Sigurdson <dbsigurd@ualberta.ca>
2018-01-27 16:29:17 -0700

    added route

--- index.js
+++ index.js
@@ -3,27 +3,24 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 var app = express();
-var router = express.Router();
+var http = require('http');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
 
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .get('/', (req, res) => res.render('pages/index'));
+
+app.get('/test', function (req, res) {
+  console.log('hit');
+  res.send('hit');
+}
+);
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // var mongo = require('mongodb');
 // var monk = require('monk');
 // var db = monk('localhost:27017/users');
 
 /* GET home page. */
-
-router.post('/test', function (req, res) {
-    console.log('hit');
-    res.send('hit');
-  }
-);
-router.get('/test', function (req, res) {
-    console.log('hit');
-    res.send('hit');
-  }
-);
