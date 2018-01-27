cc239291ae5d1ccd1f6d78f2c8bbe8eaad796e2a
Devon Sigurdson <dbsigurd@ualberta.ca>
2018-01-27 16:18:37 -0700

    updated home

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+var app = express();
+var router = express.Router();
+
+app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// var mongo = require('mongodb');
+// var monk = require('monk');
+// var db = monk('localhost:27017/users');
+
+/* GET home page. */
+
+router.post('/test', function (req, res) {
+    console.log('hit');
+    res.send('hit');
+  }
+);
